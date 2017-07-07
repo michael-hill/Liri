a938cf7017677581cc1f98ec8b0993fc86cae6ac
Michael Hill <michael-hill@live.com>
2017-07-07 17:47:51 -0500

    added keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'MkDUaR41uBlC7kLY3Y2yCZ7BH',
+  consumer_secret: 'afGVpJ83w4FWmA7YJKPYHuGPsBiJp0hl3bSZbY6BbIvOTNz6cE',
+  access_token_key: '701225099270094848-sy4Lvil3lV0yyzu4op1pIO8GilxOwsq',
+  access_token_secret: 'Qs9gwRH3w3V2YaeRvR8dAEnvfqj0aLz6WOM0W7D89C8',
+}
