93ba8068455a3bf86824ac84b6a48985727c036b
Michael Hill <michael-hill@live.com>
2017-07-17 12:38:01 -0500

    added spotify

--- liri.js
+++ liri.js
@@ -3,3 +3,13 @@ var fs = require('fs');
 var request = require('request');
 var twitter = require('twitter');
 var spotify = require('spotify');
+
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+		console.log(data);
+});
